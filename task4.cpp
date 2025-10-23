adcfb1484eba4fe0d3236eaed5b815d8733e182e
k24-0574 <k240574@nu.edu.pk>
2025-10-23 13:17:54 +0500

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -8,24 +8,25 @@ class Product{
 	string description;
 	bool availability;
 	Product(){}
-	Product(string n, double p, string d, bool a):name(n), price(p), description(d), availbility(a){}
+	Product(string n, double p, string d, bool a):name(n), price(p), description(d), availability(a){}
+    void display(){
+        cout<<name<<" - $"<<price<<endl;
+    }
 	
 };
 
-int partition (Product *arr, int start, int end){
+int partition (Product arr[], int start, int end){
 	int pivot = arr[end].price;
 	int i = start-1;
 	for (int j=start; j<end; j++){
 		if(arr[j].price<pivot){
-			Product temp(arr[])
-			
+			swap(arr[j], arr[++i]);
 		}
 	}
-		//swap
-		return i;
-		
+    swap(arr[++i], arr[end]);
+    return i;
 }
-void QuickSort(int *arr, int start, int end){
+void QuickSort(Product arr[], int start, int end){
 	if(start >=end) return;
 	int pivot = partition(arr, start, end);
 	
@@ -34,8 +35,14 @@ void QuickSort(int *arr, int start, int end){
 }
 
 int main(){
-	
-	
+    Product products[3] = 
+    {Product("Product 1", 10.99, "This is product 1", true),
+     Product("Product 2", 5.99, "This is product 2", false),
+     Product("Product 3", 2.99, "This is product 3", true)
+    };
+    QuickSort(products, 0, 2);
+    cout<<"Products sorted by price: "<<endl;
+    for(int i=0;i<3;i++) products[i].display();
+
 	return 0;
 }
-
